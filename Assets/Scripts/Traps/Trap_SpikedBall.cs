791725798f2ec0a0b10a5e3cdd862e5d8180509b
Jeevstar26 <yellowswooshx@gmail.com>
2024-06-10 21:44:30 +0200

    Trap - SpikeedBall

---
+++ Assets/Scripts/Traps/Trap_SpikedBall.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_SpikedBall : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D spikeRB;
+    [SerializeField] private float pushForce;
+    [SerializeField] private float maintainSpeedThreshold = 1f; // Seuil pour la vitesse minimale à maintenir
+
+    private void Start()
+    {
+        Vector2 pushVector = new Vector2(pushForce, 0);
+        spikeRB.AddForce(pushVector, ForceMode2D.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        // Vérifiez si la vitesse actuelle est inférieure au seuil pour réappliquer la force
+        if (Mathf.Abs(spikeRB.velocity.x) < maintainSpeedThreshold)
+        {
+            Vector2 maintainForce = new Vector2(pushForce * Mathf.Sign(spikeRB.velocity.x), 0);
+            spikeRB.AddForce(maintainForce, ForceMode2D.Force);
+        }
+    }
+}
